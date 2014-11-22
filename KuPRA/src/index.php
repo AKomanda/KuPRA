359da09e0175c7f33ed3f4bcaefc853641d22047
Uzpykes <validolislt@gmail.com>
2014-11-22 18:54:12 +0200

    Dizainas

--- KuPRA/src/index.php
+++ KuPRA/src/index.php
@@ -3,6 +3,9 @@
 <head>
 <!-- <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>  -->
 <link rel="stylesheet" type="text/css" href="styles/recepieDisplay.css">
+<link rel="stylesheet" type="text/css" href="styles/main.css">
+<link rel="stylesheet" type="text/css" href="styles/topBar.css">
+
 <!-- jQuery library (served from Google) -->
 <script
 	src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
@@ -61,13 +64,17 @@
 </head>
 
 <body>
+<div class="awesomePage">
 <?php
+include_once "display/topBar.php";
+
 include_once "display/recepieDisplay.php";
 include_once "class/databaseController.php";
 include_once "class/recepie.php";
 $user = databaseController::getDB()->get("recepto_tipai", array('tipas', '=', 2));
 recepie::getRecepie(1);
 ?>
+</div>
 </body>
 
 </html>
