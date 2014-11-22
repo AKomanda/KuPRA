e1bd446fce7996a3f3b98989902bf465154cbae4
Uzpykes <validolislt@gmail.com>
2014-11-22 18:41:34 +0200

    Dizainas

---
+++ KuPRA/src/display/topBar.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<div class="topContainer">
+	<div class="contentContainer">
+		<div class="pageLogo"></div>
+		<div class="pageMoto"></div>
+		<div class="userPane">
+			<div class="userPhoto"></div>
+			<div class="userName"></div>
+			<div class="logout"></div>
+		</div>
+	</div>
+</div>
---
+++ KuPRA/src/display/topNav.php
@@ -0,0 +1,8 @@
+<?php
+?>
+
+<div class="topNav">
+	<div class="buttons">
+		<div class="button"></div>
+	</div>
+</div>
