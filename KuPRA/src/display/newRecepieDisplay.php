35fe5ae9a5db72f90414931ec0428ec7c581eea5
Uzpykes <validolislt@gmail.com>
2014-12-04 14:31:28 +0200

    naujo recepto kurimas (soon to be finished)

--- KuPRA/src/display/newRecepieDisplay.php
+++ KuPRA/src/display/newRecepieDisplay.php
@@ -11,10 +11,11 @@
 				<fieldset class="info">
 					<label>Pavadinimas</label> <input type="text" name="recipeName" />
 					<div class="clear"></div>
-					<label>Porcijų skaičius</label> <input type="text"
-						name="recipeName" />
+					<label>Porcijų skaičius</label> <input class="short" type="text"
+						name="portions" />
 					<div class="clear"></div>
-					<label>Gaminimo laikas</label> <input type="text" name="recipeName" />
+					<label>Gaminimo laikas</label> <input class="short" type="text"
+						name="time" />&nbsp; min.
 				</fieldset>
 				<fieldset class="ingredients">
 					<label>Produktai</label>
@@ -26,8 +27,8 @@
 								<td>Kiekis</td>
 							</tr>
 						</thead>
-						<tbody>
-							<tr>
+						<tbody class="productsContainer">
+							<tr class="row">
 								<td><input type="text" name="ingredient" /></td>
 								<td><select>
 										<option>1 vienetas</option>
@@ -39,18 +40,25 @@
 						</tbody>
 					</table>
 				</fieldset>
-				<fieldset class="description"></fieldset>
-				<fieldset class="photos"></fieldset>
+				<fieldset class="description">
+					<label>Gaminimo aprašmas</label>
+					<textarea rows="5" cols="45"></textarea>
+				</fieldset>
+				<fieldset class="photos">
+					<label>Nuotraukos</label> <input type="file" />
+				</fieldset>
+				<fieldset style="float: right;" class="confirm">
+					Privatus? &nbsp;<input type="checkbox" /> <input type="submit"
+						value="Pridėti" />
+				</fieldset>
 			</div>
 			<div class="rightSide">
 				<fieldset class="type">
-				<input type="checkbox" />Karšti <br>
-				<input type="checkbox" />Šalti patiekalai<br>
-				<input type="checkbox" />Užkandžiai patiekalai<br>
-				<input type="checkbox" />random patiekalai<br>
-				<input type="checkbox" />kitas random patiekalai<br>
-				<input type="checkbox" />paskutinis random patiekalai<br>
-				
+					<input type="checkbox" />Karšti <br> <input type="checkbox" />Šalti
+					patiekalai<br> <input type="checkbox" />Užkandžiai patiekalai<br> <input
+						type="checkbox" />random patiekalai<br> <input type="checkbox" />kitas
+					random patiekalai<br> <input type="checkbox" />paskutinis random
+					patiekalai<br>
 				</fieldset>
 			</div>
 		</form>
--- KuPRA/src/styles/newrRecepie.js
+++ KuPRA/src/styles/newrRecepie.js
@@ -1,5 +1,13 @@
 $("document").ready(function() {
-	$(".newPr").click(function() {
-		$(".addProducts").before("labas<br>");
+	$(".add").click(function() {
+		$(".row").eq(-1).clone(true).appendTo(".productsContainer").find("input[type='text']").val("");
+		$(".add").eq(-2).off("click");
+		$(".add").eq(-2).bind("click", function() {
+			$(this).closest(".row").remove();
+		})
+		$(".add").eq(-2).val("-");
+		$(".add").eq(-2).attr("name", "del");
+		$(".add").eq(-2).removeClass("add").addClass("del");
 	});
+	
 });
