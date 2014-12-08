22392ee9ce4abdf166ea84cc6dec2c68300636ca
Gintas1 <gintautas.kras@gmail.com>
2014-12-08 14:20:08 +0200

    products

--- KuPRA/src/display/productsDisplay.php
+++ KuPRA/src/display/productsDisplay.php
@@ -3,27 +3,25 @@
 ?>
 <h1>Produktai</h1>
 <div class="listContainer">
-<?php
-	$products = Product::all();
-	foreach($products as $product){
-?>	
-<div class="listItemContainer">
-	<ul class="media-list">
-		<li class="madia">
-			<a class="media-left">
-				<div class="produktoNuotrauka">
-					<img src=<?php echo $product->Nuotrauka; ?>>
-				</div>
-			</a>
-			<div class="media-body">
-				<h4 class="media-heading"><?php echo $product->Pavadinimas; ?> by <a href=""><?php echo User::getUser($product->Autorius)->nick; ?></a></h4>
-				<t><?php echo $product->Aprasymas ?></t>
-			</div>
-		</li>
-	</ul>	
+	<div class="table-responsive">
+		<table class="table table-boarded table-stripped">
+			<tbody>
+			
+				<?php 
+				$products = Product::all();
+				foreach($products as $product){
+				?>
+				<tr class="listItemContainer">
+					<td><div class="produktoNuotrauka"><img src=<?php echo $product->Nuotrauka; ?>></div></td>
+					<td>
+						<h4><?php echo $product->Pavadinimas; ?></h4>
+						<a href=""><?php echo User::getUser($product->Autorius)->nick; ?></a>
+					</td>
+					<td><t><?php echo $product->Aprasymas ?></t></td>
+				</tr>
+				<?php }?>			
+			</tbody>
+		</table>
+	</div>
 </div>		
-<?php		
-	}
-?>
-</div>	
 <button type="button" class="btn btn-success" id="newProduct">Sukurti produktą</button>
